a43d5685a359ee43756d037e0f0122cb88c3861c
Brian Chen <bzchen93@gmail.com>
2022-09-07 20:50:01 +1000

    Update particle.rb

--- Formula/particle.rb
+++ Formula/particle.rb
@@ -3,9 +3,9 @@
 class Particle < Formula
   desc "Monorepo workspace manager for JS based applications"
   homepage "https://github.com/brianzchen/particle"
-  url "https://github.com/Brianzchen/particle/releases/download/v0.0.1/particle-mac.tar.gz"
-  sha256 "c41fb14b7bd826b806341002fa819c71bf3a116d59c8ca0fa25f9b2c9b3224ab"
-  version "0.0.1"
+  url "https://github.com/Brianzchen/particle/releases/download/v0.0.2/particle-mac.tar.gz"
+  sha256 "cfd7bea6e3c7c0000d2562399af6722ef174d4eda394d55c5534582f796b089e"
+  version "0.0.2"
 
   def install
     bin.install "particle"
